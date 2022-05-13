5fe5e93fdf8b9023fd658c3639f504d7e778a8f5
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-05-12 22:47:43 -0300

    app.js_metodo GET

--- app.js
+++ app.js
@@ -8,22 +8,45 @@ const app = express();
 
 //establezco los parametros de conexión db
 const conexion = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'articulosdb'
-})
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'articulosdb',
+});
 
 //probando la conexión a db
 conexion.connect((error) => {
-    if (error) {
-        throw error
-    } else {
-        console.log('conectaste la db :)')
-    }
-})
+  if (error) {
+    throw error;
+  } else {
+    console.log('conectaste la db :)');
+  }
+});
 
 app.get('/', (req, res) => res.send('Hola mundo...'));
+
+//get articulos
+app.get('/articulos', (req, res) => {
+  conexion.query('SELECT * FROM tbl_articulos', (error, data) => {
+    if (error) {
+      throw error;
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+    //get un articulo
+app.get('/articulos/:id', (req, res) => {
+  conexion.query('SELECT * FROM tbl_articulos WHERE id = ?',[req.params.id] ,  (error, data) => {
+    if (error) {
+      throw error;
+    } else {
+      res.send(data);
+      //res.send(data[0].descripcion) // data[0] es porque tenemos un solo elemento
+    }
+  });
+});
 
 //variable de entorno para que pueda variar el puerto defecto en caso de que este ocupado
 const puerto = 3000 || process.env.PUERTO;
